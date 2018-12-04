f60461890ee338b9bfbf4aeef5273fad036a606a
Jérémy Barbet <jeremgraph@gmail.com>
2018-12-04 08:55:00 +0000

    Merge version and postversion scripts together

--- post-version.sh
+++ post-version.sh
@@ -1,5 +1,6 @@
 PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]')
+perl -pi -e "s/> — v(.*)</> — v$PACKAGE_VERSION</" ./example/App.tsx
 git tag -d v$PACKAGE_VERSION
 git tag $PACKAGE_VERSION
 git add .
-git commit --amend
+git commit --amend --no-edit
--- version.sh
+++
@@ -1,2 +0,0 @@
-PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]')
-perl -pi -e "s/> — v(.*)</> — v$PACKAGE_VERSION</" ./example/App.tsx
