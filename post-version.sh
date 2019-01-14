f544668d309b10649c587cd4fb7ba6531d64259b
Jérémy Barbet <jeremgraph@gmail.com>
2019-01-14 17:04:48 +0000

    Fix post-version script

--- post-version.sh
+++ post-version.sh
@@ -4,10 +4,10 @@ PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $
 # Change dependency version on the footer component
 perl -pi -e "s/> — v(.*)/> — v$PACKAGE_VERSION/" ./examples-shared/src/components/footer/Footer.tsx
 
-# Remove default tag from npm version and recreate one without the v prefix
+# Remove default tag from npm version
 git tag -d v$PACKAGE_VERSION
-git tag $PACKAGE_VERSION
 
 # Add unstaged files and commit
 git add .
 git commit --amend --no-edit
+git tag $PACKAGE_VERSION
