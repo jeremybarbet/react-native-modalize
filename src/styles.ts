154d27d1b35e9a412c972380b10c7ee90c913e1c
Jérémy Barbet <jeremgraph@gmail.com>
2020-04-09 11:29:45 +0000

    fix: not necessary positioning

--- src/styles.ts
+++ src/styles.ts
@@ -15,12 +15,7 @@ export default StyleSheet.create({
   },
 
   modalize__wrapper: {
-    position: 'absolute',
-    top: 0,
-    right: 0,
-    bottom: 0,
-    left: 0,
-    zIndex: 9998,
+    flex: 1,
   },
 
   modalize__content: {
