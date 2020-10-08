1312d150142d006019032d7e8ca35772879179d7
Maksym Kotielnikov <catavary@gmail.com>
2020-10-08 14:47:05 +0200

    fix: position on orientation change

--- src/index.tsx
+++ src/index.tsx
@@ -848,9 +848,14 @@ const ModalizeBase = (
     },
   }));
 
+  const lastSnapRef = React.useRef(lastSnap);
   React.useEffect(() => {
-    if (alwaysOpen && (modalHeightValue || adjustToContentHeight)) {
-      handleAnimateOpen(alwaysOpen);
+    lastSnapRef.current = lastSnap;
+  }, [lastSnap]);
+
+  React.useEffect(() => {
+    if (lastSnapRef.current || (alwaysOpen && (modalHeightValue || adjustToContentHeight))) {
+      handleAnimateOpen(alwaysOpen, lastSnapRef.current ? 'default' : undefined);
     }
   }, [alwaysOpen, modalHeightValue]);
 
