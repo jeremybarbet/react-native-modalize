f17ec593b7847ba625f159f48ae27be2d7879855
Erkki Lukk <vambola@gmail.com>
2021-01-30 08:42:39 +0000

    chore: add args

--- src/utils/use-modalize.ts
+++ src/utils/use-modalize.ts
@@ -1,14 +1,15 @@
 import { useCallback, useRef } from 'react';
 import { Modalize } from '../index';
+import { TClose, TOpen } from '../options';
 
 export const useModalize = () => {
   const ref = useRef<Modalize>(null);
 
-  const close = useCallback(() => {
-    ref.current?.close();
+  const close = useCallback((dest?: TClose) => {
+    ref.current?.close(dest);
   }, []);
 
-  const open = useCallback(() => ref.current?.open(), []);
+  const open = useCallback((dest?: TOpen) => ref.current?.open(dest), []);
 
   return { ref, open, close };
 };
