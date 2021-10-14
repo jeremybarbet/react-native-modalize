4472bbe3675267d2489d6bb8b869c0da10e276d2
Erkki <vambola@gmail.com>
2021-10-14 11:29:58 +0100

    chore: utility hook for modalize (#309)

---
+++ src/utils/use-modalize.ts
@@ -0,0 +1,17 @@
+import { useCallback, useRef } from 'react';
+import { Modalize } from '../index';
+import { TClose, TOpen } from '../options';
+
+export const useModalize = () => {
+  const ref = useRef<Modalize>(null);
+
+  const close = useCallback((dest?: TClose) => {
+    ref.current?.close(dest);
+  }, []);
+
+  const open = useCallback((dest?: TOpen) => {
+    ref.current?.open(dest);
+  }, []);
+
+  return { ref, open, close };
+};
