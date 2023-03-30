86ddbcd484990b5ec9f3f03bb014534aab99c515
David <miniyoung@zamface.co.kr>
2023-03-30 11:19:09 +0900

    fix: add iphone14Pro device size and update offsetHeight

--- src/utils/devices.ts
+++ src/utils/devices.ts
@@ -3,18 +3,49 @@ import { Platform, Dimensions } from 'react-native';
 const { width, height } = Dimensions.get('window');
 
 export const isIos = Platform.OS === 'ios';
-export const isIphoneX =
-  isIos &&
-  (height === 780 ||
-    width === 780 ||
-    height === 812 ||
-    width === 812 ||
-    height === 844 ||
-    width === 844 ||
-    height === 896 ||
-    width === 896 ||
-    height === 926 ||
-    width === 926);
+
+const DEVICE_LAYOUT_MAX_VALUES: { [key: number]: string } = {
+  780: "iPhone",
+  812: "iPhoneX",
+  896: "iPhoneXMax",
+  844: "iPhone12",
+  926: "iPhone12Max",
+  852: "iPhone14Pro",
+  932: "iPhone14ProMax",
+};
+
+const targetIphoneSafeArea = (wWidth: number, wHeight: number) => {
+  const equalWidth = DEVICE_LAYOUT_MAX_VALUES[wWidth] ?? null;
+  const equalHeight = DEVICE_LAYOUT_MAX_VALUES[wHeight] ?? null;
+
+  if (equalWidth !== null) {
+    return equalWidth;
+  }
+
+  if (equalHeight !== null) {
+    return equalHeight;
+  }
+
+  return null;
+};
+
+const targetIPhoneOffsetHeight: { [key: string]: number } = {
+  iPhone: 34,
+  iPhoneX: 44,
+  iPhoneXMax: 44,
+  iPhone12: 47,
+  iPhone12Max: 47,
+  iPhone14Pro: 59,
+  iPhone14ProMax: 59,
+};
+
+export const iphoneOffsetHeight = () => {
+  const iphoneType = targetIphoneSafeArea(width, height);
+
+  return isIos && iphoneType !== null
+    ? targetIPhoneOffsetHeight[iphoneType]
+    : 0;
+};
 
 export const isAndroid = Platform.OS === 'android';
 export const isWeb = Platform.OS === 'web';
